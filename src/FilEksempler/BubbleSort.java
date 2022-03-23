a334fcb822615ba38befc31f79861e6eff5ca5af
susannejung <95688233+susannejung@users.noreply.github.com>
2022-03-23 07:36:46 +0100

    Opdatering 210322

--- src/FilEksempler/BubbleSort.java
+++ src/FilEksempler/BubbleSort.java
@@ -4,15 +4,16 @@ class BubbleSort <E extends Comparable<E>> {
     /**
      * Bubble sort method
      */
-    public void bubbleSort(E[] list, objektComparator c) {
+     public void bubbleSort(E[] list, objektComparator c) {
+
         boolean needNextPass = true;
 
         for (int k = 1; k < list.length && needNextPass; k++) {
             // Array may be sorted and next pass not needed
             needNextPass = false;
             for (int i = 0; i < list.length - k; i++) {
-                if (c.compare(list[i], list[i + 1]) > 0) {
-                    // Swap list[i] with list[i + 1]
+               if (c.compare(list[i], list[i + 1]) > 0) {
+                 // Swap list[i] with list[i + 1]
                     E temp = list[i];
                     list[i] = list[i + 1];
                     list[i + 1] = temp;
--- src/FilEksempler/Main.java
+++ src/FilEksempler/Main.java
@@ -5,7 +5,7 @@ public class Main {
      * A test method
      */
     public static void main(String[] args) {
-        objektComparator nc1 = new objektComparator();
+      /*  objektComparator nc1 = new objektComparator();
         Integer[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
         BubbleSort b2 = new BubbleSort();
         b2.bubbleSort(list, nc1);
@@ -43,6 +43,33 @@ public class Main {
         for (int i = 0; i < tabel1.length; i++)
             System.out.print(tabel1[i] + " ");
         System.out.println();
+        */
+
+        String[] tabel = {"Bent", "Anne", "Anna", "Peter", "Jens", "Bodil"};
+        BubbleSort1 b1=new BubbleSort1();
+        b1.bubbleSort(tabel);
+        for (int i = 0; i < tabel.length; i++)
+            System.out.print(tabel[i] + " ");
+        System.out.println();
+
+        Character[] list1 = {'d', 'e', 'a', 'c', 'b', 'g', 'h', 'p', 'o', 'l'};
+        b1.bubbleSort(list1);
+        for (int i = 0; i < list1.length; i++)
+            System.out.print(list1[i] + " ");
+        System.out.println();
+
+        Student[] tabel1 = new Student[6];
+        tabel1[0] = (new Student(1, "Bent", 17));
+        tabel1[1] = (new Student(2, "Anne", 20));
+        tabel1[2] = (new Student(3, "Anna", 20));
+        tabel1[3] = (new Student(4, "Peter", 22));
+        tabel1[4] = (new Student(5, "Jens", 25));
+        tabel1[5] = (new Student(6, "Bodil", 22));
+        b1.bubbleSort(tabel1);
+        for (int i = 0; i < tabel1.length; i++)
+            System.out.print(tabel1[i] + " ");
+        System.out.println();
+
 
     }
 }
